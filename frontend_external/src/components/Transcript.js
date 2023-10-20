c238890bf7e1cb0bf594d67b0f8dff7291bb11bd
Donte Small <donte.small@zoom.us>
2023-10-20 02:30:31 -0400

    Fixed Typo

--- frontend_external/src/components/Transcript.js
+++ frontend_external/src/components/Transcript.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 
+// Define your styled components outside of the functional component
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  background-color: rgb(3, 0, 31);
+  height: 100vh;
+  color: white;
+  font-size: 14px;
+  position: sticky;
+  top: 0;
+`;
+
+const Wrapper = styled.div`
+  padding: 18px 65px;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 35px;
+  font-weight: bold;
+  margin-bottom: 25px;
+
+  &:hover {
+    background-color: #373737;
+  }
+`;
 
 function Transcript() {
-  const Container = styled.div`
-    display: flex;
-    justify-content: flex-end; /* Move content to the right side */
-    background-color: rgb(3, 0, 31);
-    height: 100vh;
-    color: white;
-    font-size: 14px;
-    position: sticky;
-    top: 0;
-  `;
-
-  const Wrapper = styled.div`
-    padding: 18px 65px; /* Fixed a typo in the padding value */
-  `;
-
-  const Logo = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 35px;
-    font-weight: bold;
-    margin-bottom: 25px;
-
-    &:hover {
-      background-color: #373737;
-  `;
-
   return (
     <>
       <Container>
         <Wrapper>
           <Logo>
-          
             Zoom Mtg AI Transcript
           </Logo>
         </Wrapper>
